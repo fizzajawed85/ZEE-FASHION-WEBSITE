4a5e1ea06696301317e3222dc86b2b1f04385ff7
fizzajawed85 <angelfizza85@gmail.com>
2025-06-29 01:33:00 +0500

    ZEE FASHION WEBSITE / HTML, TAILWAIND CSS

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+// side bar toogle icon 
+
+   function toggleFilter(id) {
+    const options = document.getElementById(`filter-options-${id}`);
+    const icon = document.getElementById(`toggle-icon-${id}`);
+    if (options.classList.contains('hidden')) {
+      options.classList.remove('hidden');
+      icon.textContent = '-';
+    } else {
+      options.classList.add('hidden');
+      icon.textContent = '+';
+    }
+  };
+
+  function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown-options");
+    dropdown.classList.toggle("hidden");
+  }
+
+  function selectOption(value) {
+    document.getElementById("selected-option").innerText = value;
+    document.getElementById("dropdown-options").classList.add("hidden");
+  }
+
+  // Optional: Close dropdown when clicking outside
+  window.addEventListener("click", function (e) {
+    const dropdown = document.getElementById("custom-dropdown");
+    if (!dropdown.contains(e.target)) {
+      document.getElementById("dropdown-options").classList.add("hidden");
+    }
+  });
